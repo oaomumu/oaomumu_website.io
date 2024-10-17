d98d9d2eef6efbb6ea254bad591dde89f1569ec4
松懈 <58425836+oaomumu@users.noreply.github.com>
2024-10-17 20:47:22 +0800

    update

--- js/index.js
+++ js/index.js
@@ -1,1 +1,7 @@
-document.getElementsByTagName("h1")[0].style.fontSize = "6vw";
+fetch('IMG_songxie.txt')
+    .then(response => response.text())
+    .then(data => {
+        const imgElement = document.getElementById('myImage');
+        imgElement.src = data; // 设置图片的 src 属性
+    })
+    .catch(error => console.error('Error loading image:', error));
